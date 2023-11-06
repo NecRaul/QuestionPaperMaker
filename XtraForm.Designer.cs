250398e9addc57c73ab219d737296e5055175632
NecRaul <necraul2001@gmail.com>
2023-11-06 23:13:46 +0400

    Base Form

---
+++ XtraForm.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace QuestionPaperMaker
+{
+    partial class XtraForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            textEdit3 = new DevExpress.XtraEditors.TextEdit();
+            textEdit4 = new DevExpress.XtraEditors.TextEdit();
+            textEdit5 = new DevExpress.XtraEditors.TextEdit();
+            btnLoad = new DevExpress.XtraEditors.SimpleButton();
+            labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            btnPreview = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)textEdit1.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit2.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit3.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit4.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit5.Properties).BeginInit();
+            SuspendLayout();
+            // 
+            // textEdit1
+            // 
+            textEdit1.Location = new Point(141, 44);
+            textEdit1.Name = "textEdit1";
+            textEdit1.Size = new Size(100, 20);
+            textEdit1.TabIndex = 0;
+            // 
+            // textEdit2
+            // 
+            textEdit2.Location = new Point(141, 70);
+            textEdit2.Name = "textEdit2";
+            textEdit2.Size = new Size(100, 20);
+            textEdit2.TabIndex = 1;
+            // 
+            // textEdit3
+            // 
+            textEdit3.Location = new Point(141, 96);
+            textEdit3.Name = "textEdit3";
+            textEdit3.Size = new Size(100, 20);
+            textEdit3.TabIndex = 2;
+            // 
+            // textEdit4
+            // 
+            textEdit4.Location = new Point(141, 122);
+            textEdit4.Name = "textEdit4";
+            textEdit4.Size = new Size(100, 20);
+            textEdit4.TabIndex = 3;
+            // 
+            // textEdit5
+            // 
+            textEdit5.Location = new Point(141, 148);
+            textEdit5.Name = "textEdit5";
+            textEdit5.Size = new Size(100, 20);
+            textEdit5.TabIndex = 4;
+            // 
+            // btnLoad
+            // 
+            btnLoad.Location = new Point(39, 175);
+            btnLoad.Name = "btnLoad";
+            btnLoad.Size = new Size(98, 23);
+            btnLoad.TabIndex = 5;
+            btnLoad.Text = "Sualları yüklə";
+            // 
+            // labelControl1
+            // 
+            labelControl1.Location = new Point(100, 47);
+            labelControl1.Name = "labelControl1";
+            labelControl1.Size = new Size(35, 13);
+            labelControl1.TabIndex = 6;
+            labelControl1.Text = "Fakültə";
+            // 
+            // labelControl2
+            // 
+            labelControl2.Location = new Point(95, 73);
+            labelControl2.Name = "labelControl2";
+            labelControl2.Size = new Size(40, 13);
+            labelControl2.TabIndex = 7;
+            labelControl2.Text = "Qrup №";
+            // 
+            // labelControl3
+            // 
+            labelControl3.Location = new Point(86, 99);
+            labelControl3.Name = "labelControl3";
+            labelControl3.Size = new Size(49, 13);
+            labelControl3.TabIndex = 8;
+            labelControl3.Text = "Fənnin adı";
+            // 
+            // labelControl4
+            // 
+            labelControl4.Location = new Point(39, 125);
+            labelControl4.Name = "labelControl4";
+            labelControl4.Size = new Size(96, 13);
+            labelControl4.TabIndex = 9;
+            labelControl4.Text = "İmtahan qəbul edən";
+            // 
+            // labelControl5
+            // 
+            labelControl5.Location = new Point(73, 151);
+            labelControl5.Name = "labelControl5";
+            labelControl5.Size = new Size(62, 13);
+            labelControl5.TabIndex = 10;
+            labelControl5.Text = "İl və semestr";
+            // 
+            // btnPreview
+            // 
+            btnPreview.Location = new Point(143, 175);
+            btnPreview.Name = "btnPreview";
+            btnPreview.Size = new Size(98, 23);
+            btnPreview.TabIndex = 11;
+            btnPreview.Text = "Biletləri yarat";
+            // 
+            // XtraForm
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(298, 268);
+            Controls.Add(btnPreview);
+            Controls.Add(labelControl5);
+            Controls.Add(labelControl4);
+            Controls.Add(labelControl3);
+            Controls.Add(labelControl2);
+            Controls.Add(labelControl1);
+            Controls.Add(btnLoad);
+            Controls.Add(textEdit5);
+            Controls.Add(textEdit4);
+            Controls.Add(textEdit3);
+            Controls.Add(textEdit2);
+            Controls.Add(textEdit1);
+            Name = "XtraForm";
+            Text = "XtraForm";
+            ((System.ComponentModel.ISupportInitialize)textEdit1.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit2.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit3.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit4.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textEdit5.Properties).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit textEdit3;
+        private DevExpress.XtraEditors.TextEdit textEdit4;
+        private DevExpress.XtraEditors.TextEdit textEdit5;
+        private DevExpress.XtraEditors.SimpleButton btnLoad;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.SimpleButton btnPreview;
+    }
+}
