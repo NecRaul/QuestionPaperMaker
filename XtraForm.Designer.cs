c7762af8e65bf3ff01466ef4278508e5076f861b
NecRaul <necraul@kuroneko.dev>
2023-11-06 23:28:08 +0400

    Loading correct arguments

--- XtraForm.Designer.cs
+++ XtraForm.Designer.cs
@@ -28,11 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textEdit1 = new DevExpress.XtraEditors.TextEdit();
-            textEdit2 = new DevExpress.XtraEditors.TextEdit();
-            textEdit3 = new DevExpress.XtraEditors.TextEdit();
-            textEdit4 = new DevExpress.XtraEditors.TextEdit();
-            textEdit5 = new DevExpress.XtraEditors.TextEdit();
+            txtFaculty = new DevExpress.XtraEditors.TextEdit();
+            txtGroup = new DevExpress.XtraEditors.TextEdit();
+            txtClass = new DevExpress.XtraEditors.TextEdit();
+            txtTeacher = new DevExpress.XtraEditors.TextEdit();
+            txtYearAndSemester = new DevExpress.XtraEditors.TextEdit();
             btnLoad = new DevExpress.XtraEditors.SimpleButton();
             labelControl1 = new DevExpress.XtraEditors.LabelControl();
             labelControl2 = new DevExpress.XtraEditors.LabelControl();
@@ -40,47 +40,47 @@
             labelControl4 = new DevExpress.XtraEditors.LabelControl();
             labelControl5 = new DevExpress.XtraEditors.LabelControl();
             btnPreview = new DevExpress.XtraEditors.SimpleButton();
-            ((System.ComponentModel.ISupportInitialize)textEdit1.Properties).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit2.Properties).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit3.Properties).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit4.Properties).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit5.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)txtFaculty.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)txtGroup.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)txtClass.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)txtTeacher.Properties).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)txtYearAndSemester.Properties).BeginInit();
             SuspendLayout();
             // 
-            // textEdit1
+            // txtFaculty
             // 
-            textEdit1.Location = new Point(141, 44);
-            textEdit1.Name = "textEdit1";
-            textEdit1.Size = new Size(100, 20);
-            textEdit1.TabIndex = 0;
+            txtFaculty.Location = new Point(141, 44);
+            txtFaculty.Name = "txtFaculty";
+            txtFaculty.Size = new Size(100, 20);
+            txtFaculty.TabIndex = 0;
             // 
-            // textEdit2
+            // txtGroup
             // 
-            textEdit2.Location = new Point(141, 70);
-            textEdit2.Name = "textEdit2";
-            textEdit2.Size = new Size(100, 20);
-            textEdit2.TabIndex = 1;
+            txtGroup.Location = new Point(141, 70);
+            txtGroup.Name = "txtGroup";
+            txtGroup.Size = new Size(100, 20);
+            txtGroup.TabIndex = 1;
             // 
-            // textEdit3
+            // txtClass
             // 
-            textEdit3.Location = new Point(141, 96);
-            textEdit3.Name = "textEdit3";
-            textEdit3.Size = new Size(100, 20);
-            textEdit3.TabIndex = 2;
+            txtClass.Location = new Point(141, 96);
+            txtClass.Name = "txtClass";
+            txtClass.Size = new Size(100, 20);
+            txtClass.TabIndex = 2;
             // 
-            // textEdit4
+            // txtTeacher
             // 
-            textEdit4.Location = new Point(141, 122);
-            textEdit4.Name = "textEdit4";
-            textEdit4.Size = new Size(100, 20);
-            textEdit4.TabIndex = 3;
+            txtTeacher.Location = new Point(141, 122);
+            txtTeacher.Name = "txtTeacher";
+            txtTeacher.Size = new Size(100, 20);
+            txtTeacher.TabIndex = 3;
             // 
-            // textEdit5
+            // txtYearAndSemester
             // 
-            textEdit5.Location = new Point(141, 148);
-            textEdit5.Name = "textEdit5";
-            textEdit5.Size = new Size(100, 20);
-            textEdit5.TabIndex = 4;
+            txtYearAndSemester.Location = new Point(141, 148);
+            txtYearAndSemester.Name = "txtYearAndSemester";
+            txtYearAndSemester.Size = new Size(100, 20);
+            txtYearAndSemester.TabIndex = 4;
             // 
             // btnLoad
             // 
@@ -152,29 +152,29 @@
             Controls.Add(labelControl2);
             Controls.Add(labelControl1);
             Controls.Add(btnLoad);
-            Controls.Add(textEdit5);
-            Controls.Add(textEdit4);
-            Controls.Add(textEdit3);
-            Controls.Add(textEdit2);
-            Controls.Add(textEdit1);
+            Controls.Add(txtYearAndSemester);
+            Controls.Add(txtTeacher);
+            Controls.Add(txtClass);
+            Controls.Add(txtGroup);
+            Controls.Add(txtFaculty);
             Name = "XtraForm";
             Text = "XtraForm";
-            ((System.ComponentModel.ISupportInitialize)textEdit1.Properties).EndInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit2.Properties).EndInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit3.Properties).EndInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit4.Properties).EndInit();
-            ((System.ComponentModel.ISupportInitialize)textEdit5.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)txtFaculty.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)txtGroup.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)txtClass.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)txtTeacher.Properties).EndInit();
+            ((System.ComponentModel.ISupportInitialize)txtYearAndSemester.Properties).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private DevExpress.XtraEditors.TextEdit textEdit1;
-        private DevExpress.XtraEditors.TextEdit textEdit2;
-        private DevExpress.XtraEditors.TextEdit textEdit3;
-        private DevExpress.XtraEditors.TextEdit textEdit4;
-        private DevExpress.XtraEditors.TextEdit textEdit5;
+        private DevExpress.XtraEditors.TextEdit txtFaculty;
+        private DevExpress.XtraEditors.TextEdit txtGroup;
+        private DevExpress.XtraEditors.TextEdit txtClass;
+        private DevExpress.XtraEditors.TextEdit txtTeacher;
+        private DevExpress.XtraEditors.TextEdit txtYearAndSemester;
         private DevExpress.XtraEditors.SimpleButton btnLoad;
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.LabelControl labelControl2;
